283a3cc2fbc5048753d82c593567d767128ce3d3
Juanmi Taboada <juanmi@centrologic.com>
2017-07-26 22:44:27 +0200

    Working on it

--- codenerix_pos/consumers.py
+++ codenerix_pos/consumers.py
@@ -1,139 +1,169 @@
-import time
 import json
 import uuid
 import pyotp
 import base64
 import hashlib
-import threading
 
-from channels import Channel
 from channels.generic.websockets import JsonWebsocketConsumer
 
 from codenerix.lib.debugger import Debugger
 from codenerix_pos.models import POS
 
-def send_error(self, msg):
-    # Send the challenge
-    self.error("ERROR: {}".format(msg))
-    self.send({
-            "action": "error",
-            "error": msg,
-        })
 
-def request_to_authenticate(self):
-    self.warning("Request to authenticate")
+class POSConsumer(JsonWebsocketConsumer, Debugger):
 
-    # Create a new challenge
-    challenge = uuid.uuid4().hex
+    # We want sessions
+    http_user = True
 
-    # Remember it in out session
-    if self.message.channel_session.get("challenge", None) is None:
-        self.message.channel_session["challenge"] = challenge
-        print("CHALLENGE")
-    else:
-        print("GO")
+    # Set to True if you want it, else leave it out
+    # strict_ordering = True
 
-    # Send the challenge
-    self.message.reply_channel.send({
+    def __init__(self, *args, **kwargs):
 
-        # Accept the connection; this is done by default if you don't override the connect function.
-        "accept": True,
+        # Initialize Debugger
+        self.set_name("POSConsumer")
+        self.set_debug()
 
-        "text": json.dumps({
-            "action": "authenticate",
-            "version": "1.0",
-            "challenge": challenge
-            }
-        )})
+        # Let it keep going normal
+        super(POSConsumer, self).__init__(*args, **kwargs)
 
-def check_authentication_request(self, message):
-    # Prepare the basic answer for this service
-    answer = {"action": "authenticated"}
+    @property
+    def authenticated(self):
+        return self.message.channel_session.get("POS", False)
 
-    # Get data from the package
-    cid = message.get('id', '')
+    def connect(self, message, **kwargs):
+        """
+        Perform things on connection start
+        """
+        self.debug("New connection", color="blue")
+        self.request_to_authenticate()
 
-    # Try to locate the POS in the database
-    pos = POS.objects.filter(cid=cid).first()
+    def receive(self, message, **kwargs):
+        """
+        Called when a message is received with decoded JSON content
+        """
+        self.debug("")
+        self.debug("Receive:{} - {}".format(message, self.authenticated), color="cyan")
+        self.debug("    session: {}".format(self.message.channel_session.__dict__), color='purple')
+        self.debug("       user: {}".format(self.message.user), color='purple')
 
-    if pos:
-        # If we found it, get the token from the package
-        token = message.get('token', '')
+        # Get action
+        if type(message) is dict:
+            action = message.get('action', None)
 
-        # Read the challenge key we sent to our client
-        challenge = self.message.channel_session["challenge"]
+            if action is not None:
 
-        # Build a temporaly hash
-        hashkey = "{}{}".format(challenge, pos.token)
-        hash32 = base64.b32encode(bytes(hashkey, 'utf-8'))
-        totp = pyotp.TOTP(hash32).now()
-        localtoken = hashlib.sha1(bytes("{}{}".format(hashkey, totp), 'utf-8')).hexdigest()
-
-        # Check if our hash match our client's hash
-        auth = localtoken == token
-    else:
-        # Not found in the database, not authorized!
-        auth = False
-
-    # Write result
-    answer['result'] = auth
-
-    # Check if we should add some hardware information
-    if auth:
-        # Remember the POS
-        self.message.channel_session["POS"] = pos
-        # Get all the hardware connected to this POS
-        answer['hardware'] = []
-        for hw in pos.hardwares.all():
-            # Prepare to send back the config
-            answer['hardware'].append({'kind': hw.kind, 'config': hw.config})
-
-        # Show authenticated answer
-        print("Send:{}".format(answer), color='green')
-    else:
-        # Show not authenticated answer
-        print("Send:{}".format(answer), color='red')
-
-    # Send the authentication answer
-    self.send(answer)
-
-
-
-def ws_message(message):
-    """
-    Called when a message is received with decoded JSON content
-    """
-    print("")
-    print("Receive:{} - {}".format(message, authenticated))
-    print("    session: {}".format(message.channel_session._session_cache))
-
-    # Get action
-    if type(message) is dict:
-        action = message.get('action', None)
-
-        if action is not None:
-
-            # Check the action that it is requesting
-            if action == 'authenticate':
-                # User is requesting to be authenticated, let's do it
-                check_authentication_request(message)
-            else:
-                # The user is requesting another action, if authenticated, let it go!
-                if authenticated:
-                    # Choose the users action
-                    if action in ['known']:
-                        # Everything is fine, keep going!
-                        print("Keep going")
-                    else:
-                        # Unknown action
-                        send_error("Unknown action '{}'".format(action))
+                # Check the action that it is requesting
+                if action == 'authenticate':
+                    # User is requesting to be authenticated, let's do it
+                    self.check_authentication_request(message)
                 else:
-                    # Push the user to authenticate
-                    request_to_authenticate()
+                    # The user is requesting another action, if authenticated, let it go!
+                    if self.authenticated:
+                        # Choose the users action
+                        if action in ['known']:
+                            # Everything is fine, keep going!
+                            self.debug("Keep going", color="green")
+                        else:
+                            # Unknown action
+                            self.send_error("Unknown action '{}'".format(action))
+                    else:
+                        # Push the user to authenticate
+                        self.request_to_authenticate()
+            else:
+                # No action
+                self.send_error("No action specified")
         else:
-            # No action
-            send_error("No action specified")
-    else:
-        # Not a dictionary
-        send_error("This server only accepts dictionaries")
+            # Not a dictionary
+            self.send_error("This server only accepts dictionaries")
 
+    def disconnect(self, message, **kwargs):
+        """
+        Perform things on connection close
+        """
+        self.warning("Client got disconnected")
+
+    def send_error(self, msg):
+        # Send the challenge
+        self.error("ERROR: {}".format(msg))
+        self.send({
+                "action": "error",
+                "error": msg,
+            })
+
+    def request_to_authenticate(self):
+        self.warning("Request to authenticate")
+
+        # Remember it in out session
+        challenge = self.message.channel_session.get("challenge", None)
+        if challenge is None:
+            # Create a new challenge
+            challenge = uuid.uuid4().hex
+            self.message.channel_session["challenge"] = challenge
+            self.debug("NEW CHALLENGE: {}".format(challenge), color="red")
+        else:
+            self.debug("GO WITH EXISTING CHALLENGE: {}".format(challenge), color="green")
+
+        # Send the challenge
+        self.message.reply_channel.send({
+
+            # Accept the connection; this is done by default if you don't override the connect function.
+            "accept": True,
+
+            "text": json.dumps({
+                "action": "authenticate",
+                "version": "1.0",
+                "challenge": challenge
+                }
+            )})
+
+    def check_authentication_request(self, message):
+        # Prepare the basic answer for this service
+        answer = {"action": "authenticated"}
+
+        # Get data from the package
+        cid = message.get('id', '')
+
+        # Try to locate the POS in the database
+        pos = POS.objects.filter(cid=cid).first()
+
+        if pos:
+            # If we found it, get the token from the package
+            token = message.get('token', '')
+
+            # Read the challenge key we sent to our client
+            challenge = self.message.channel_session["challenge"]
+
+            # Build a temporaly hash
+            hashkey = "{}{}".format(challenge, pos.token)
+            hash32 = base64.b32encode(bytes(hashkey, 'utf-8'))
+            totp = pyotp.TOTP(hash32).now()
+            localtoken = hashlib.sha1(bytes("{}{}".format(hashkey, totp), 'utf-8')).hexdigest()
+
+            # Check if our hash match our client's hash
+            auth = localtoken == token
+        else:
+            # Not found in the database, not authorized!
+            auth = False
+
+        # Write result
+        answer['result'] = auth
+
+        # Check if we should add some hardware information
+        if auth:
+            # Remember the POS
+            self.message.channel_session["POS"] = pos
+            # Get all the hardware connected to this POS
+            answer['hardware'] = []
+            for hw in pos.hardwares.all():
+                # Prepare to send back the config
+                answer['hardware'].append({'kind': hw.kind, 'config': hw.config})
+
+            # Show authenticated answer
+            self.debug("Send:{}".format(answer), color='green')
+        else:
+            # Show not authenticated answer
+            self.debug("Send:{}".format(answer), color='red')
+
+        # Send the authentication answer
+        self.send(answer)
--- codenerix_pos/routing.py
+++ codenerix_pos/routing.py
@@ -1,11 +1,13 @@
-# from channels import route_class
-# from codenerix_pos.consumers import POSConsumer
-from channels.routing import route
-from codenerix_pos.consumers import ws_message
+from channels import route_class
+from codenerix_pos.consumers import POSConsumer
+# from channels.routing import route
+# from codenerix_pos.consumers import ws_message, ws_connect, ws_disconnect
 
-# channel_routing = [
-#    route_class(POSConsumer, path=r"^/codenerix_pos/"),
-# ]
 channel_routing = [
-    route("websocket.receive", ws_message),
+    route_class(POSConsumer, path=r"^/codenerix_pos/"),
 ]
+# channel_routing = [
+#    route("websocket.connect", ws_connect, path=r"^\/codenerix_pos\/(?P<uid>[a-zA-Z0-9_]+)$"),
+#    route("websocket.receive", ws_message, path=r"^/codenerix_pos/(?P<uid>[a-zA-Z0-9_]+)$"),
+#    route("websocket.disconnect", ws_disconnect, path=r"^/codenerix_pos/(?P<uid>[a-zA-Z0-9_]+)$"),
+# ]
